2c6f39d2ef9782194b883f3c2427382de6ef977b
lukewellerbg <luke.weller@berkshiregrey.com>
2023-03-02 10:45:45 -0500

    better logic, removed partial match edge case bug

--- wordle.py
+++ wordle.py
@@ -25,28 +25,46 @@ def check_guess(guess, answer):
 
 	yellow_chars_dict = defaultdict(int)
 	bad_chars = set()
-	print_map = []
+	print_map = ['','','','','']
 
+	# As far as I can tell, we MUST iterate through the answer twice - once for exact 
+	# matches and once for partial matches.  With only one iteration, it's possible to 
+	# reach an edge case where an exact match for a char occurs after a partial match, 
+	# and we double mark the first char bc/ the alg hasn't yet seen the exact match.
+	# --------------------------------------------------------------------------------
+	# For example, if the answer is bukes, and out guess is supes, using only one iteration
+	# will result in the first 's' in supes being marked a partial match.  This is incorrect,
+	# since the only 's' in bukes is already accounted for with the exact match at the end.
+	
 	for i in range(5):
 		if guess[i] == answer[i]:
 			answer_dict[guess[i]] -= 1
-			print_map.append('g')
+			print_map[i] = 'g'
 
-		elif guess[i] in answer_dict:
-			if yellow_chars_dict[guess[i]] < answer_dict[guess[i]]:
-				yellow_chars_dict[guess[i]] += 1
-				print_map.append('y')
-			else:
-				print_map.append('x')
-		
+	for i in range(5):
+		# Skip already discovered matches
+		if print_map[i] == 'g':
+			continue
 		else:
-			bad_chars.add(guess[i])
-			print_map.append('x')
+			if guess[i] in answer_dict:
+				if yellow_chars_dict[guess[i]] < answer_dict[guess[i]]:
+					yellow_chars_dict[guess[i]] += 1
+					print_map[i] = 'y'
+				else:
+					print_map[i] = 'x'
+			
+			else:
+				bad_chars.add(guess[i])
+				print_map[i] = 'x'
+
+	if '' in print_map:
+		sys.exit('error: unable to parse print_map')
 
 	print_guess(guess, print_map)
 
 	if guess == answer:
-		sys.exit('Correct!')
+		print('\tCorrect!')
+		sys.exit(0)
 
 	return bad_chars
 
